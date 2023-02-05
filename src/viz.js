6ebd982746cc7bdd8abb652f0c86b6f1c40af6da
Dirk Brockmannn <brockmann.dirk@gmail.com>
2023-02-05 20:29:02 +0100

    working on different turtle shapes

--- src/viz.js
+++ src/viz.js
@@ -13,6 +13,20 @@ const paint = colors[syscfg.simulation.colormap];
 var W, H, ctx, cfg;
 var ctx;
 
+var line = d3.line().x(d=>param.agentsize*d.x).y(d=>param.agentsize*d.y);	
+
+function tadpole () {
+	var M = 30;
+	var drop = d3.range(M).map(function(d,i){
+			return { 
+				x: -2 * Math.cos(i/M*Math.PI*2), 
+				y: Math.sin(i/M*Math.PI*2) * Math.pow( Math.sin(i/M/2*Math.PI*2) , 6 )
+			};
+		})
+	return line(drop);
+}	
+
+console.log(tadpole())
 
 const initialize = (display,config) => {
 	
@@ -32,10 +46,18 @@ const initialize = (display,config) => {
 	ctx.strokeRect(0, 0, cfg.display_size.width, cfg.display_size.height);		
 
 	agents.forEach(a=>{
+		//ctx.save()
 		 ctx.beginPath();
- 		ctx.arc(X(a.x), Y(a.y), param.agentsize, 0, 2 * Math.PI, false);
+		 ctx.moveTo(0,0)
+		 ctx.lineTo(X(10),Y(0))
+ 		ctx.rotate(a.theta * Math.PI / 180);
+		ctx.translate(X(a.x),Y(a.y))
+		//ctx.arc(X(a.x), Y(a.y), param.agentsize, 0, 2 * Math.PI, false);
+		
+		ctx.stroke();
  		ctx.fillStyle = param.color_by_heading.widget.value() ? paint(a.theta_neighbors/360)  : "black";
  		ctx.fill();
+		//ctx.restore();
 	})
 	
 };
